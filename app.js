19c046125eb7130bc43a9a5446b3ef0c152c35f0
IrinaJ <irina.ul87@gmail.com>
2022-08-25 17:58:49 -0400

    Html and js added

---
+++ app.js
@@ -0,0 +1,14 @@
+const letters = document.querySelectorAll('span');
+
+function rgbColors() {
+    let r  = Math.floor(Math.random() * 256);
+    let g  = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+    return `rgb(${r},${g},${b})`;
+}
+
+setInterval( function() {
+    for (let letter of letters) {
+        letter.style.color = rgbColors();
+    }
+}, 1000);
